a5b51930d49a11ceba24124d34dc7d8999e0d9dd
AnastasiiaTkachuk <anastasiia.tkachuk07@gmail.com>
2025-05-06 14:20:55 +0200

    add README.md

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your first number:");
+        double firstNumber = double.Parse(Console.ReadLine());
+        Console.WriteLine("Write your second number:");
+        double secondNumber = double.Parse(Console.ReadLine());
+        Console.WriteLine("Write your third number:");
+        double thirdNumber = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine(HeronsFormula(firstNumber, secondNumber, thirdNumber));
+    }
 
-Console.WriteLine("Hello, World!");
+    static double HeronsFormula(double a, double b, double c)
+    {
+        double p = (a + b + c) / 2;
+        double ans = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        return ans;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# Homework: Heron's Formula
+
+Ask the user to input the lengths of the three sides of a triangle. The lengths can be decimal (floating-point) values.
+
+After receiving the side lengths, use Heron's formula to calculate the area of the triangle.
+
+To make things a bit more interesting, find the formula yourself.
+
+Once the area is calculated, display the result in the console.
